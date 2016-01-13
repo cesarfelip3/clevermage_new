d05d976a5d6f4ba59ea52e0230e7379eb0e2fd48
Samuel Adam <samuel@agilizelabs.com>
2016-01-13 18:49:45 +0545

    add email template

--- app/code/local/Zing/Contactus/controllers/IndexController.php
+++ app/code/local/Zing/Contactus/controllers/IndexController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Mirasvit
  *
@@ -13,51 +14,36 @@
  * @build     1709
  * @copyright Copyright (C) 2015 Mirasvit (http://mirasvit.com/)
  */
-
-
-
 class Zing_Contactus_IndexController extends Mage_Core_Controller_Front_Action
 {
+
     public function indexAction()
     {
         echo __CLASS__ . __FUNCTION__;
     }
-    
+
     public function postAction()
     {
-        $fromEmail = $this->getRequest()->getParam('mail');
-	$fromName = $this->getRequest()->getParam('name');
-	
-	$toEmail = "itmyprofession@gmail.com";
-	$toName = "Cesar Felipe";
-	
-        $body = "Telphone : " . $this->getRequest()->getParam('telephone') . "\n";
-	$body .= 'Comment : ' .$this->getRequest()->getParam('comment');
+        $emailTemplateVariables = $this->getRequest()->getParams();
         
-	$subject = $this->getRequest()->getParam('subject');
+        $emailTemplate = Mage::getModel('core/email_template')
+                ->loadDefault('zing_custom_email_template1');
         
-	$mail = new Zend_Mail();		
-	
-	$mail->setBodyHtml($body);
-	
-	$mail->setFrom($fromEmail, $fromName);
-	
-	$mail->addTo($toEmail, $toName);
-	
-	$mail->setSubject($subject);
-	
-	try {
-            echo __FILE__;die;
-		$mail->send();
-                echo __FILE__;die;
-	}
-	catch(Exception $ex) {
-		// I assume you have your custom module. 
-		// If not, you may keep 'customer' instead of 'yourmodule'.
-		Mage::getSingleton('core/session')
-			->addError(Mage::helper('zing_contactus')
-			->__('Unable to send email.'));
-	}
+        $processedTemplate = $emailTemplate->getProcessedTemplate($emailTemplateVariables);
+        
+        $emailTemplate->send('itmyprofession@gmail.com','Santosh Moktan', $emailTemplateVariables);
+
+        try {
+            $mail->send();
+        }
+        catch (\Exception $ex) {
+            // I assume you have your custom module. 
+            // If not, you may keep 'customer' instead of 'yourmodule'.
+            Mage::getSingleton('core/session')
+                    ->addError(Mage::helper('zing_contactus')
+                            ->__('Unable to send email.'));
+        }
         echo "Success";
     }
+
 }
